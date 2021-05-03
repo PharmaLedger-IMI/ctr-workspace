fadb7a3dac0acfd7c814ac1f74951bd18056bcc3
jpsl <joao.luis@pdmfc.com>
2021-05-03 17:17:31 +0100

    #2 - patient-ssapp improved

---
+++ ctr-patient/ctr-dsu-wizard/constants.js
@@ -0,0 +1,12 @@
+const ANCHORING_DOMAIN = "ctr";
+const INFO_PATH = require('../pdm-dsu-toolkit/constants').INFO_PATH;
+const PARTICIPANT_MOUNT_PATH = require('../pdm-dsu-toolkit/constants').PARTICIPANT_MOUNT_PATH;
+
+const DEFAULT_QUERY_OPTIONS = require('../pdm-dsu-toolkit/constants').DEFAULT_QUERY_OPTIONS;
+
+module.exports = {
+    ANCHORING_DOMAIN,
+    DEFAULT_QUERY_OPTIONS,
+    INFO_PATH,
+    PARTICIPANT_MOUNT_PATH
+}
---
+++ ctr-patient/ctr-dsu-wizard/services/index.js
@@ -0,0 +1,11 @@
+/**
+ * @module ctr-dsu-wizard.services
+ */
+module.exports = {
+    DSUService: require('../../pdm-dsu-toolkit/services/DSUService'),
+    ParticipantService: require('./ParticipantService'),
+    WebcLocaleService: require("../../pdm-dsu-toolkit/services/WebcLocaleService"),
+    WebComponentService: require("../../pdm-dsu-toolkit/services/WebComponentService"),
+    Strategy: require("../../pdm-dsu-toolkit/services/strategy"),
+    utils: require('../../pdm-dsu-toolkit/services/utils')
+}
