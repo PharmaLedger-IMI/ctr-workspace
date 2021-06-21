6274a3fb0c1436ab2414a4041ea48925c4aabeb8
Joao Luis <joao.luis@pdmfc.com>
2021-06-21 22:20:46 +0100

    #6 - matchrequests 4 forms displayed using lforms-29.0.3

--- ctr-backoffice-frontend/src/app/matchrequest-detail/matchrequest-detail.component.ts
+++ ctr-backoffice-frontend/src/app/matchrequest-detail/matchrequest-detail.component.ts
@@ -15,7 +15,10 @@ import { MatchRequestService } from '../matchrequest.service';
 export class MatchRequestDetailComponent implements OnInit {
 
     @Input() mr?: MatchRequest;
-    @ViewChild('formContainer') formContainer?: ElementRef;
+    @ViewChild('ghiForm')    ghiForm?:    ElementRef;
+    @ViewChild('trialPrefs') trialPrefs?: ElementRef;
+    @ViewChild('condition')  condition?:  ElementRef;
+    @ViewChild('trial')      trial?:      ElementRef;
 
     LForms: any;
 
@@ -27,6 +30,13 @@ export class MatchRequestDetailComponent implements OnInit {
         private window: Window
     ) {
         this.LForms = (this.window as any)["LForms"];
+        /* LForms notes - using as non-angular
+           angular.json 
+               architect.build.styles
+                   "src/assets/lforms-29.0.3/styles/lforms.min.css"
+               architect.build.scripts
+                   "src/assets/lforms-29.0.3/lforms.min.js"   
+        */
     }
 
     ngOnInit(): void {
@@ -39,25 +49,44 @@ export class MatchRequestDetailComponent implements OnInit {
         const keySSI = this.route.snapshot.paramMap.get('keyssi');
         if (!keySSI) {
             console.log("request keyssi is null");
+            this.refreshMr(undefined);
             return;
         }
         this.mrService.getMatchRequest(keySSI)
             .subscribe(mr => { this.refreshMr(mr); });
     }
 
-    refreshMr(mr: MatchRequest) {
+    refreshMr(mr?: MatchRequest) {
         console.log("Window.LForms", this.LForms);
-        console.log("formContainer", this.formContainer);
         console.log("RefreshMR", mr);
         this.mr = mr;
-        if (this.formContainer) {
-            this.formContainer!.nativeElement.innerHTML = "empty MR";
-            if (this.mr?.dsuData?.ghiForm) {
-                this.formContainer!.nativeElement.innerHTML = "ok2";
-                this.LForms.Util.addFormToPage(this.mr?.dsuData?.ghiForm, this.formContainer.nativeElement, {});
-            } else {
-                console.log("NO GHI", this.mr);
-            }
+        if (!this.ghiForm)
+            return;
+        this.ghiForm!.nativeElement.innerHTML = "empty MR";
+        if (!this.trialPrefs)
+            return;
+        this.trialPrefs!.nativeElement.innerHTML = "empty MR";
+        if (!this.condition)
+            return;
+        this.condition!.nativeElement.innerHTML = "empty MR";
+        if (!this.trial)
+            return;
+        this.trial!.nativeElement.innerHTML = "empty MR";
+        if (this.mr?.dsuData?.ghiForm) {
+            this.ghiForm!.nativeElement.innerHTML = "init...";
+            this.LForms.Util.addFormToPage(this.mr?.dsuData?.ghiForm, this.ghiForm.nativeElement, {});
+        }
+        if (this.mr?.dsuData?.trialPrefs) {
+            this.trialPrefs!.nativeElement.innerHTML = "init...";
+            this.LForms.Util.addFormToPage(this.mr?.dsuData?.trialPrefs, this.trialPrefs.nativeElement, {});
+        }
+        if (this.mr?.dsuData?.condition) {
+            this.condition!.nativeElement.innerHTML = "init...";
+            this.LForms.Util.addFormToPage(this.mr?.dsuData?.condition, this.condition.nativeElement, {});
+        }
+        if (this.mr?.dsuData?.trial) {
+            this.trial!.nativeElement.innerHTML = "init...";
+            this.LForms.Util.addFormToPage(this.mr?.dsuData?.trial, this.trial.nativeElement, {});
         }
     }
 
