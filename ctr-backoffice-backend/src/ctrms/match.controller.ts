a2b1bad820e4373c03e69d0626e56387b337eedf
jpsl <joao.luis@pdmfc.com>
2021-07-06 12:20:49 +0100

    #5 - store trialPrefs and matchRequest response on file to debug lforms

--- ctr-backoffice-backend/src/ctrms/match.controller.ts
+++ ctr-backoffice-backend/src/ctrms/match.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Request, Body, Post, UnauthorizedException, Param } from '@nestjs/common';
 import { ApiBearerAuth, ApiBody, ApiInternalServerErrorResponse, ApiOkResponse, ApiTags, ApiUnauthorizedResponse, getSchemaPath } from "@nestjs/swagger";
+import { fstat } from 'fs';
 import { ClinicalTrial } from 'src/ctrial/clinicaltrial.entity';
 import { ClinicalTrialQuery } from 'src/ctrial/clinicaltrialquery.validator';
 import { PaginatedDto } from 'src/paginated.dto';
@@ -51,6 +52,16 @@ export class MatchController {
         console.log("/ctrms/trialPrefs req.body =", req.body);
         let res = await this.matchService.trialPrefs(req.body);
         console.log("/ctrms/trialPrefs res =", res);
+        // TODO: comment to prevent write of files to temp dir
+        const fs = require("fs");
+        const os = require("os");
+        const path = require('path');
+        const tempDir = os.tmpdir(); // /tmp
+        const epochNow = Date.now();
+        const fileName = "trialPrefs"+epochNow+".json";
+        const fullFileName = path.join(tempDir, fileName);
+        fs.writeFileSync(fullFileName, JSON.stringify(res));
+        console.log("/ctrms/trialPrefs response stored at ", fullFileName);
         return res;
     }
 
@@ -68,6 +79,15 @@ export class MatchController {
         console.log("/ctrms/submit req.body =", req.body);
         let res = this.matchService.submit(req.body);
         console.log("/ctrms/submit res =", res);
+        const fs = require("fs");
+        const os = require("os");
+        const path = require('path');
+        const tempDir = os.tmpdir(); // /tmp
+        const epochNow = Date.now();
+        const fileName = "trialPrefs"+epochNow+".json";
+        const fullFileName = path.join(tempDir, fileName);
+        fs.writeFileSync(fullFileName, JSON.stringify(res));
+        console.log("/ctrms/submit response stored at ", fullFileName);
         return res;
     }
 }
