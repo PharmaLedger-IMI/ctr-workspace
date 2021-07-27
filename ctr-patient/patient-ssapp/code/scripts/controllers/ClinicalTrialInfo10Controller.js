c4440aa166efb754dedbfa519a23224684839276
Pedro Costa <pedro.c.costa@pdmfc.com>
2021-07-27 06:14:15 -0300

    refs #21 - add display a map for clinical site address

--- ctr-patient/patient-ssapp/code/scripts/controllers/ClinicalTrialInfo10Controller.js
+++ ctr-patient/patient-ssapp/code/scripts/controllers/ClinicalTrialInfo10Controller.js
@@ -9,6 +9,8 @@ export default class ClinicalTrialInfo10Controller extends LocalizedController {
         ctr: { name: "?" }
     }); // uninitialized blank model
 
+    map = undefined;
+
     constructor(element, history) {
         super(element, history);
         const wizard = require('wizard');
@@ -28,6 +30,36 @@ export default class ClinicalTrialInfo10Controller extends LocalizedController {
             //console.log("ctr", self.model.ctr);
             //console.log("condition", self.model.ctr.clinicalTrialMedicalConditions[0].medicalCondition.name);
             self.setState(undefined);
+
+            if (this.map !== undefined) {
+                try {
+                    this.map.off();
+                    this.map.remove();
+                } catch (e) {
+                    console.error('clinicalTrial.map error ', e);
+                }
+            }
+            const location = self.model.ctr.clinicalSite.address.location;
+            this.map = this.buildMap(location.latitude, location.longitude);
+            setTimeout(() => this.map.invalidateSize(), 100);
         }, {capture: true});
+    }
+
+    buildMap(lat, long) {
+        const baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        });
+        const map = L.map('clinical-site-map', {
+            center: [lat, long],
+            zoom: 15,
+            minZoom: 12,
+            maxZoom: 17,
+            dragging: false,
+            scrollWheelZoom: 'center',
+            layers: [baseLayer]
+        });
+        L.marker([lat, long]).addTo(map);
+        map.setMaxBounds([[-90, -180], [90, 180]]);
+        return map;
     }
 }
