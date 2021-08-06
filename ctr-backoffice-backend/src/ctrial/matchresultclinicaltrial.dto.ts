eddaa3616ab6a4435cabc6fd803fe4d2ce61a608
Joao Luis <joao.luis@pdmfc.com>
2021-08-06 21:22:01 +0100

    #27 - missing commit of matchresultclinicaltrial.dto.ts

--- ctr-backoffice-backend/src/ctrial/matchresultclinicaltrial.dto.ts
+++ ctr-backoffice-backend/src/ctrial/matchresultclinicaltrial.dto.ts
@@ -6,8 +6,10 @@ export class MatchResultClinicalTrial {
         // the MatchRequest+MatchResult
 
     criteriaCount: number = 0; // total count of criteria evaluated for this trial
-
+    
     criteriaMatchedCount: number = 0; // total count of criteria matched/pass for this trial
+
+    criteriaConfidenceCount: number = 0; // total count of criteria that is relevant for the match confidence %
     
     constructor(ctr: any) {
         this.clinicalTrial = ctr;
