314ac9c564f25acb14bf13457236b05139d77616
Joao Luis <joao.luis@pdmfc.com>
2021-08-07 19:50:37 +0100

    #27 - on view matchrequestnew50complete.html sort matchresults by ascendant travel distance, and then by descendant match confidence

--- ctr-patient/patient-ssapp/code/scripts/controllers/MatchRequestNew50CompleteController.js
+++ ctr-patient/patient-ssapp/code/scripts/controllers/MatchRequestNew50CompleteController.js
@@ -39,15 +39,27 @@ export default class MatchRequestNew50CompleteController extends LocalizedContro
                 && self.match.matchResult.trials
                 && Array.isArray(self.match.matchResult.trials)
             ) {
+                let mtctCollection = [];
                 self.match.matchResult.trials.forEach((mtct) => {
                     if (mtct.criteriaMatchedCount >= mtct.criteriaCount) {
                         mtct.matchConfidenceToDisplay = ((mtct.criteriaConfidenceCount / mtct.criteriaCount)*100.0).toFixed(1); // webcardinal seems unable to support complex @expressions so we calculate it here.
                         if (mtct.clinicalTrial.travDistMiles) {
                             mtct.clinicalTrial.travDistKm = (Math.round(mtct.clinicalTrial.travDistMiles * 1.60934 * 100) / 100).toFixed(2);
                         }
-                        self.model.matchedTrials.push(mtct);
+                        mtctCollection.push(mtct);
                     }
                 });
+                // sort by ascending travelDistanceKm, and then by descendant matchConfidenceToDisplay
+                mtctCollection.sort((mtct1, mtct2) => {
+                    if (mtct1.clinicalTrial.travDistKm && mtct2.clinicalTrial.travDistKm) {
+                        let travDistKmDiff = mtct1.clinicalTrial.travDistKm - mtct2.clinicalTrial.travDistKm;
+                        if (travDistKmDiff != 0) {
+                            return travDistKmDiff;
+                        }
+                    }
+                    return mtct2.matchConfidenceToDisplay - mtct1.matchConfidenceToDisplay;
+                });
+                self.model.matchedTrials = mtctCollection;
             }
         }, {capture: true});
 
