241ce5cd0c76674874240cacf7ebf492fdc02cbe
jpsl <joao.luis@pdmfc.com>
2021-09-22 19:44:52 +0100

    #50 - patient application listing pagination

--- ctr-backoffice-frontend/src/app/application/application.component.ts
+++ ctr-backoffice-frontend/src/app/application/application.component.ts
@@ -1,4 +1,6 @@
+import { AotCompiler } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Application } from '../application';
 import { ApplicationService } from '../application.service';
@@ -15,6 +17,8 @@ export class ApplicationComponent implements OnInit {
 
   paginatedApps: PaginatedDto<ApplicationQuery, Application> = { count: 0, query: new ApplicationQuery(), results: []};
   
+  displayedColumns: string[] = ['name', 'email', 'createdOn'];
+
   constructor(
     private appService: ApplicationService,
     public authService: AuthService,
@@ -23,10 +27,23 @@ export class ApplicationComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    const appQuery = new ApplicationQuery();
+    appQuery.clinicalSite = this.authService.getClinicalSiteId();
+    //appQuery.sponsor = this.authService.getSponsorId(); // TODO
     this.appService.getAll(new ApplicationQuery()).subscribe((results) => {
       console.log("getAll", results);
       this.paginatedApps = results;
     });
   }
 
+  changePage(event: PageEvent) {
+    // calling api function here with the offset
+    const appQuery = new ApplicationQuery();
+    appQuery.limit = event.pageSize;
+    appQuery.page = event.pageIndex;
+    this.appService.getAll(appQuery).subscribe((results) => {
+      console.log("getAll", results);
+      this.paginatedApps = results;
+    });
+  }
 }
