6a0415abce0c8831720d777940a9f8f431358c4d
Pedro Costa <pedro.c.costa@pdmfc.com>
2021-09-23 09:49:11 -0300

    refs #50 - add /ctrms/apply to manager and middleware

--- ctr-patient/ctr-dsu-wizard/constants.js
+++ ctr-patient/ctr-dsu-wizard/constants.js
@@ -5,6 +5,7 @@ const ENV_URL_DEV2_PDM = "https://ctr2-dev.pharmaledger.pdmfc.com";
 const ENV_URL_TST_PDM = "https://ctr.pharmaledger.pdmfc.com";
 const ENV_URL_TST2_PDM = "https://ctr2.pharmaledger.pdmfc.com";
 const INFO_PATH = require('../pdm-dsu-toolkit/constants').INFO_PATH;
+const MATCH_REQUEST_APPLY_API_HUB_ENDPOINT = '/ctr-match-service/apply#x-blockchain-domain-request'; // the '#x-blockchain-domain-request' fragment is needed to bypass the service worker
 const MATCH_REQUEST_TRIALFIND_API_HUB_ENDPOINT = '/ctr-match-service/trialFind#x-blockchain-domain-request'; // the '#x-blockchain-domain-request' fragment is needed to bypass the service worker
 const MATCH_REQUEST_TRIALPREFS_API_HUB_ENDPOINT = '/ctr-match-service/trialPrefs#x-blockchain-domain-request'; // the '#x-blockchain-domain-request' fragment is needed to bypass the service worker
 const MATCH_REQUEST_SUBMIT_API_HUB_ENDPOINT = '/ctr-match-service/submit#x-blockchain-domain-request'; // the '#x-blockchain-domain-request' fragment is needed to bypass the service worker
@@ -32,6 +33,7 @@ module.exports = {
     ENV_URL_TST_PDM,
     ENV_URL_TST2_PDM,
     INFO_PATH,
+    MATCH_REQUEST_APPLY_API_HUB_ENDPOINT,
     MATCH_REQUEST_TRIALFIND_API_HUB_ENDPOINT,
     MATCH_REQUEST_TRIALPREFS_API_HUB_ENDPOINT,
     MATCH_REQUEST_SUBMIT_API_HUB_ENDPOINT,
