0606b2adb564c5dffafb0b0a58f1b6dee15d802c
jpsl <joao.luis@pdmfc.com>
2022-09-06 16:46:27 +0100

    #99 - store lastUser credentials

--- ctr-patient/pdm-trust-loader/src/scripts/controllers/HomeController.js
+++ ctr-patient/pdm-trust-loader/src/scripts/controllers/HomeController.js
@@ -18,15 +18,24 @@ export default class HomeController extends LocalizedController {
         super(element, history);
         super.bindLocale(this);
         this.model = this.initializeModel();
+        // see src/pages/home.translate.json for a definition of the form fields
 
         let self = this;
-        self.model.formJSON = JSON.stringify(self.model.form.login);
+        const lastUser = self.getLastUser();
+        if (lastUser) {
+            console.log("login form for", lastUser);
+            self.model.formJSON = JSON.stringify(lastUser);
+        } else {
+            console.log("register form", self.model.form.register);
+            self.model.formJSON = JSON.stringify(self.model.form.register);
+        }
         this.loaderService = new LoaderService(env);
 
         self.on('click', async (evt) => {
             const {id} = evt.target;
             if (id === 'loginView') {
-                self.model.formJSON = JSON.stringify(self.model.form.login);
+                const lastUser = self.getLastUser();
+                self.model.formJSON = JSON.stringify(lastUser ? lastUser : self.model.form.login);
             }
             if (id === 'signUpView') {
                 self.model.formJSON = JSON.stringify(self.model.form.register);
@@ -41,13 +50,14 @@ export default class HomeController extends LocalizedController {
             const credentialsForm = action === 'login' ? self.model.form.login : self.model.form.register;
 
             const credentials = Object.keys(form).reduce((accum, name) => {
-                if (name !== 'terms-and-conditions') {
+                if (name !== 'terms-and-conditions' && name !== 'password-repeat') { // remove these fields from credentials
                     const isPublic = credentialsForm.fields.find(f => f.name === name).public;
                     accum[name] = {secret: form[name], public: isPublic};
                 }
                 return accum;
             }, {});
 
+            console.log("credentials", credentials);
             const method = action === 'login' ? self.login : self.register;
             await method.call(self, credentials, (err, result) => {
                 if (err) {
@@ -57,7 +67,7 @@ export default class HomeController extends LocalizedController {
             });
         }, true);
 
-        console.log("Home controller initialized");
+        console.log("Home controller initialized", lastUser);
     }
 
     /**
@@ -94,16 +104,9 @@ export default class HomeController extends LocalizedController {
             } else {
                 self.showToast(self.translate('success.register'));
                 setTimeout(() => {
-                    Object.keys(credentials).forEach((key) => {
-                        self.model.form.login.fields.forEach((field) => {
-                            if (field.name === key) {
-                                field.props.value = credentials[key].secret;
-                            }
-                        });
-                    });
-                    self.model.formJSON = JSON.stringify(self.model.form.login);
-                    }, 1300
-                );
+                    const lastUser = self.storeCredentialsAsLastUser(credentials);
+                    self.model.formJSON = JSON.stringify(lastUser);
+                }, 1300);
             }
             await loader.dismiss();
             callback(undefined, keySSI);
@@ -126,7 +129,51 @@ export default class HomeController extends LocalizedController {
                 return callback(err);
             }
             self.showToast(self.translate('success.login'));
+            self.storeCredentialsAsLastUser(credentials);
             callback(undefined, wallet);
         });
+    }
+
+    /**
+     * Copy credentials into login fields, and store as lastUser.
+     * @param {*} credentials 
+     * @returns an object that can be used as a login form.
+     */
+    storeCredentialsAsLastUser(credentials) {
+        const self = this;
+        Object.keys(credentials).forEach((key) => {
+            self.model.form.login.fields.forEach((field) => {
+                if (field.name === key) {
+                    if (key === "password") {
+                        // store password as blank (forcing the user to input the password the next time)
+                        field.props.value = "";                        
+                    } else {
+                        field.props.value = credentials[key].secret;
+                    }
+                }
+            });
+        });
+        const lastUser = self.model.form.login;
+        self.setLastUser(lastUser);
+        return lastUser;
+    }
+
+    /**
+     * Returns the last login form for a signed up userd. See the register method above.
+     * @returns undefined if never used. The JSON.parse of the form object data if already used.
+     */
+    getLastUser() {
+        const lastUserItem = localStorage.getItem("user");
+        if (!lastUserItem)
+            return lastUserItem;
+        return JSON.parse(lastUserItem);
+    }
+
+    /**
+     * Record the object for the last user that signed up properly.
+     * @param {*} userObj An object that can be used as a login form.
+     */
+    setLastUser(userObj) {
+        localStorage.setItem("user", JSON.stringify(userObj));
     }
 }
